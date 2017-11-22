6b7a3377f96e1b1e407abc6aca856bc9ef7fb802
John Lees <lees.john6@gmail.com>
2017-11-22 17:17:46 -0500

    Add script to calculate distances from phylogeny
    More species with lots of MGEs, likely more accurate than mash if using core genome. Can also remove recombination etc.
    
    Have included topology dist for possible future comparisons.
    C mat should not be used, but might be in future.

---
+++ scripts/phylogeny_distance.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# Copyright 2017 Marco Galardini and John Lees
+
+'''Extract a distance matrix from a phylogeny'''
+
+
+def get_options():
+    import argparse
+
+    description = 'Extract a distance matrix from a phylogeny'
+    parser = argparse.ArgumentParser(description=description)
+
+    parser.add_argument('phylogeny',
+                        help='Tree file')
+
+    parser.add_argument('--format',
+                        default="newick",
+                        help="Format of tree file")
+    parser.add_argument('--calc-C',
+                        action='store_true',
+                        help='Produce var-covar matrix C (as from PDDIST). Always uses branch lengths.')
+    parser.add_argument('--topology',
+                        action='store_true',
+                        default=False,
+                        help='Ignore branch lengths, and only use topological distances')
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    options = get_options()
+
+    import sys
+    import re
+    import pandas as pd
+    import dendropy
+
+    tree = dendropy.Tree.get(
+        path=options.phylogeny,
+        schema=options.format,
+        preserve_underscores=True)
+
+    d = {}
+    pdm = tree.phylogenetic_distance_matrix()
+    for idx1, taxon1 in enumerate(tree.taxon_namespace):
+        d[taxon1.label] = d.get(taxon1.label, {})
+        for taxon2 in tree.taxon_namespace:
+            if taxon2.label not in d[taxon1.label].keys():
+                if options.calc_C:
+                    mrca = pdm.mrca(taxon1, taxon2)
+                    d[taxon1.label][taxon2.label] = mrca.distance_from_root()
+                elif options.topology:
+                    d[taxon1.label][taxon2.label] = pdm.path_edge_count(taxon1, taxon2)
+                else:
+                    d[taxon1.label][taxon2.label] = pdm.patristic_distance(taxon1, taxon2)
+
+    m = pd.DataFrame(d)
+    m.to_csv(sys.stdout,
+             sep='\t')
+
