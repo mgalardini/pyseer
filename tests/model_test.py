1d99a83294d17112f70a82317ef0666e480ace12
Marco Galardini <galactus8403@gmail.com>
2018-01-26 17:18:56 +0000

    More model tests

--- tests/model_test.py
+++ tests/model_test.py
@@ -1,8 +1,12 @@
 import warnings
 import unittest
 import numpy as np
+import pandas as pd
 from scipy import stats
+import statsmodels.formula.api as smf
 from pyseer.model import pre_filtering
+from pyseer.model import fit_null
+from pyseer.model import fit_firth
 
 
 np.random.seed(42)
@@ -41,6 +45,58 @@ class TestPreFiltering(unittest.TestCase):
         prep, bad_chisq = pre_filtering(p, k, True)
         self.assertEqual(prep, 8.6308642007939013e-30)
         self.assertFalse(bad_chisq)
+
+
+class TestFitNull(unittest.TestCase):
+    def test_fit_null_binary(self):
+        p = np.random.randint(2, size=100)
+        m = np.random.random(size=(100, 10))
+        cov = pd.DataFrame([])
+        v = np.concatenate((np.ones(100).reshape(-1, 1),
+                            m),
+                           axis=1)
+        start_vec = np.zeros(v.shape[1])
+        start_vec[0] = np.log(np.mean(p)/(1-np.mean(p)))
+        null_mod = smf.Logit(p, v)
+        null_test = null_mod.fit(start_params=start_vec,
+                                 method='newton',
+                                 disp=False)
+        # no covariates
+        null_res = fit_null(p, m, cov, False, firth=False)
+        self.assertTrue(abs((null_test.params - null_res.params).max()) < 1E-15)
+        self.assertAlmostEqual(null_test.llr_pvalue, null_res.llr_pvalue)
+        # no covariates, firth regression
+        (intercept, kbeta, beta, bse, fitll) = fit_firth(null_mod,
+                                                         start_vec,
+                                                         'null',
+                                                         v, p)
+        null_test = fitll
+        null_res = fit_null(p, m, cov, False, firth=True)
+        self.assertAlmostEqual(null_test, null_res)
+        # covariates
+        cov = pd.DataFrame(np.random.random(size=(100, 3)))
+        v = np.concatenate((np.ones(100).reshape(-1, 1),
+                            m,
+                            cov),
+                           axis=1)
+        start_vec = np.zeros(v.shape[1])
+        start_vec[0] = np.log(np.mean(p)/(1-np.mean(p)))
+        null_mod = smf.Logit(p, v)
+        null_test = null_mod.fit(start_params=start_vec,
+                                 method='newton',
+                                 disp=False)
+        null_res = fit_null(p, m, cov, False, firth=False)
+        self.assertTrue(abs((null_test.params - null_res.params).max()) < 1E-15)
+        self.assertAlmostEqual(null_test.llr_pvalue, null_res.llr_pvalue)
+        # covariates, firth regression
+        (intercept, kbeta, beta, bse, fitll) = fit_firth(null_mod,
+                                                         start_vec,
+                                                         'null',
+                                                         v, p)
+        null_test = fitll
+        null_res = fit_null(p, m, cov, False, firth=True)
+        self.assertAlmostEqual(null_test, null_res)
+
 
 if __name__ == '__main__':
     unittest.main()
