6c8590a5a7c70e7754a93e5b4cc54c2364923de9
Marco Galardini <galactus8403@gmail.com>
2018-03-07 21:31:51 +0000

    Version bump
    Also added a changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# CHANGELOG
+v1.0.1
+- Check for missing values in VCF entries
+
+v1.0.0 (Feb 2018)
+- First release
--- pyseer/__init__.py
+++ pyseer/__init__.py
@@ -2,4 +2,4 @@
 
 '''Python reimplementation of SEER for bacterial GWAS'''
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
