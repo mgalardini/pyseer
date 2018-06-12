d893d123474e02d9569f666e6a4e8e83d70b31b5
John Lees <lees.john6@gmail.com>
2018-06-12 09:44:41 -0400

    Correct BWA header reader when >1 sequence in ref

--- pyseer/kmer_mapping/bwa.py
+++ pyseer/kmer_mapping/bwa.py
@@ -49,12 +49,13 @@ def bwa_iter(reference, fasta, algorithm):
     bwa_p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=devnull, shell=True, universal_newlines=True)
     # read sam file from bwa mem
     if algorithm == "mem":
-        # discard header
-        bwa_p.stdout.readline()
-        bwa_p.stdout.readline()
-
         for sam_line in bwa_p.stdout:
             sam_fields = sam_line.rstrip().split("\t")
+
+            # discard header
+            if sam_fields[0][0] == "@":
+                continue
+
             positions = []
             if int(sam_fields[1]) & 4 == 4:
                 mapped = False
